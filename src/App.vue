e9a2be592e5583db8a770ed32d3f9520663188a1
Adrián Maza <adri_mv1@hotmail.com>
2024-04-18 17:36:33 +0200

    vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
+
+const displayName = ref("");
+
+const setName = (e) => {
+  displayName.value = e;
+}
+</script>
+
+<template>
+  <h1>Component events | Comunicación hijo-padre ↑</h1>
+
+  <h2>Hola {{ displayName }}</h2>
+
+  <ChildComponent @login="setName"></ChildComponent>
+</template>
+
+<style></style>
