1dfbcf6ada03549ca20056ab50f09a7bed6846c4
Adrián Maza <adri_mv1@hotmail.com>
2024-12-18 12:04:19 +0100

    02 variables & reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import NavBar from "./components/NavBar.vue";
+import { ref } from "vue";
+
+let number = ref(0);
+
+setInterval(() => {
+  number.value++;
+}, 1000);
 </script>
 
 <template>
-  <h1 class="title">Hola a todos!</h1>
-  <NavBar></NavBar>
+  <h1>Hola a todos!</h1>
+  <h2>{{ number }}</h2>
 </template>
 
-<style>
-.title {
-  background-color: blue;
-  color: white;
-}
-</style>
+<style></style>
